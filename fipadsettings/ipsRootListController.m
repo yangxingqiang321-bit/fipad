10941baaf08d3e838e89172d923f789310e076be
yangxingqiang321-bit <yangxingqiang321@gmail.com>
2026-08-08 18:33:22 +0800

    创建 ipsRootListController.m

---
+++ fipadsettings/ipsRootListController.m
@@ -0,0 +1,34 @@
+#include "ipsRootListController.h"
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <rootless.h>
+#import "NSTask.h"
+
+@implementation ipsRootListController
+
+- (NSArray *)specifiers {
+	if (!_specifiers) {
+		_specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
+	}
+	return _specifiers;
+}
+
+-(void)sbreload
+{
+	UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"fipad"
+							message:@"设置已保存\n现在 Respring 吗？"
+							preferredStyle:UIAlertControllerStyleActionSheet];
+	UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"否" style:UIAlertActionStyleCancel
+		handler:^(UIAlertAction * action) {}];
+	UIAlertAction* yes = [UIAlertAction actionWithTitle:@"是" style:UIAlertActionStyleDestructive
+		handler:^(UIAlertAction * action) {
+			NSTask *t = [[NSTask alloc] init];
+			[t setLaunchPath:ROOT_PATH_NS(@"/usr/bin/sbreload")];
+			[t launch];
+		}];
+	[alert addAction:defaultAction];
+	[alert addAction:yes];
+	[self presentViewController:alert animated:YES completion:nil];
+}
+
+@end
