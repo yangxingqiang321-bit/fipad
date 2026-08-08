f93ab292c424fda8fbf1df9dfa9f73895c2a4648
yangxingqiang321-bit <yangxingqiang321@gmail.com>
2026-08-09 05:44:58 +0800

    更新 ipsRootListController.m

--- fipadsettings/ipsRootListController.m
+++ fipadsettings/ipsRootListController.m
@@ -2,10 +2,7 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #import <rootless.h>
-#import <objc/runtime.h>
 #import "NSTask.h"
-
-NSString *const domainString = @"com.schlub51.fipad";
 
 @implementation ipsRootListController
 
@@ -16,147 +13,6 @@ NSString *const domainString = @"com.schlub51.fipad";
     return _specifiers;
 }
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    self.title = @"fipad";
-    [self setupCustomUI];
-}
-
-- (void)setupCustomUI {
-    CGFloat screenWidth = self.view.bounds.size.width;
-    CGFloat margin = 20;
-    CGFloat contentWidth = screenWidth - margin * 2;
-    
-    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
-    scrollView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    scrollView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentAutomatic;
-    [self.view addSubview:scrollView];
-    
-    UIView *contentView = [[UIView alloc] init];
-    [scrollView addSubview:contentView];
-    
-    CGFloat y = 0;
-    
-    y = [self addSectionLabel:@"竖屏设置" atY:y width:contentWidth toView:contentView];
-    y = [self addSliderWithStepper:@"卡片缩放" key:@"cardScale" min:0.20 max:0.50 step:0.01 atY:y width:contentWidth toView:contentView];
-    y = [self addSliderWithStepper:@"垂直间距" key:@"vertSpacingPort" min:10 max:120 step:1 atY:y width:contentWidth toView:contentView];
-    y = [self addSliderWithStepper:@"水平间距" key:@"horizSpacingPort" min:10 max:120 step:1 atY:y width:contentWidth toView:contentView];
-    
-    y += 10;
-    y = [self addSectionLabel:@"横屏设置" atY:y width:contentWidth toView:contentView];
-    y = [self addSliderWithStepper:@"卡片缩放" key:@"cardScaleLandscape" min:0.20 max:0.50 step:0.01 atY:y width:contentWidth toView:contentView];
-    y = [self addSliderWithStepper:@"垂直间距" key:@"vertSpacingLand" min:10 max:120 step:1 atY:y width:contentWidth toView:contentView];
-    y = [self addSliderWithStepper:@"水平间距" key:@"horizSpacingLand" min:10 max:120 step:1 atY:y width:contentWidth toView:contentView];
-    
-    y += 10;
-    y = [self addSectionLabel:@"卡片圆角" atY:y width:contentWidth toView:contentView];
-    y = [self addSliderWithStepper:@"圆角半径" key:@"cornerRadius" min:0 max:40 step:1 atY:y width:contentWidth toView:contentView];
-    
-    y += 30;
-    UIButton *applyBtn = [UIButton buttonWithType:UIButtonTypeSystem];
-    applyBtn.frame = CGRectMake(0, y, contentWidth, 44);
-    [applyBtn setTitle:@"应用更改" forState:UIControlStateNormal];
-    applyBtn.backgroundColor = [UIColor systemBlueColor];
-    applyBtn.layer.cornerRadius = 10;
-    [applyBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    applyBtn.titleLabel.font = [UIFont boldSystemFontOfSize:16];
-    [applyBtn addTarget:self action:@selector(sbreload) forControlEvents:UIControlEventTouchUpInside];
-    [contentView addSubview:applyBtn];
-    y += 70;
-    
-    contentView.frame = CGRectMake(margin, 0, contentWidth, y);
-    scrollView.contentSize = CGSizeMake(contentWidth, y + 20);
-}
-
-- (CGFloat)addSectionLabel:(NSString *)text atY:(CGFloat)y width:(CGFloat)width toView:(UIView *)view {
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, y, width, 30)];
-    label.text = text;
-    label.font = [UIFont boldSystemFontOfSize:16];
-    label.textColor = [UIColor systemGrayColor];
-    [view addSubview:label];
-    return y + 30;
-}
-
-- (CGFloat)addSliderWithStepper:(NSString *)labelText
-                             key:(NSString *)key
-                             min:(CGFloat)min
-                             max:(CGFloat)max
-                            step:(CGFloat)step
-                             atY:(CGFloat)y
-                           width:(CGFloat)width
-                          toView:(UIView *)view {
-    // 标签
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, y, 85, 30)];
-    label.text = labelText;
-    label.font = [UIFont systemFontOfSize:14];
-    [view addSubview:label];
-    
-    NSUserDefaults *defaults = [[NSUserDefaults alloc] initWithSuiteName:domainString];
-    CGFloat currentValue = [defaults floatForKey:key];
-    if (currentValue == 0) currentValue = (min + max) / 2;
-    
-    // 滑块
-    CGFloat sliderX = 90;
-    CGFloat sliderWidth = width - sliderX - 70 - 5 - 5;
-    UISlider *slider = [[UISlider alloc] initWithFrame:CGRectMake(sliderX, y, sliderWidth, 30)];
-    slider.minimumValue = min;
-    slider.maximumValue = max;
-    slider.value = currentValue;
-    slider.accessibilityLabel = key;
-    [slider addTarget:self action:@selector(sliderChanged:) forControlEvents:UIControlEventValueChanged];
-    [view addSubview:slider];
-    
-    // 数值显示
-    CGFloat valueX = sliderX + sliderWidth + 5;
-    UILabel *valueLabel = [[UILabel alloc] initWithFrame:CGRectMake(valueX, y, 45, 30)];
-    valueLabel.text = [NSString stringWithFormat:@"%.2f", currentValue];
-    valueLabel.font = [UIFont systemFontOfSize:14];
-    valueLabel.textAlignment = NSTextAlignmentCenter;
-    [view addSubview:valueLabel];
-    
-    // 步进器
-    CGFloat stepperX = valueX + 50;
-    UIStepper *stepper = [[UIStepper alloc] initWithFrame:CGRectMake(stepperX, y, 80, 30)];
-    stepper.minimumValue = min;
-    stepper.maximumValue = max;
-    stepper.stepValue = step;
-    stepper.value = currentValue;
-    stepper.accessibilityLabel = key;
-    [stepper addTarget:self action:@selector(stepperChanged:) forControlEvents:UIControlEventValueChanged];
-    [view addSubview:stepper];
-    
-    objc_setAssociatedObject(slider, "valueLabel", valueLabel, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-    objc_setAssociatedObject(stepper, "valueLabel", valueLabel, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-    
-    return y + 40;
-}
-
-- (void)sliderChanged:(UISlider *)sender {
-    NSString *key = sender.accessibilityLabel;
-    UILabel *valueLabel = objc_getAssociatedObject(sender, "valueLabel");
-    NSUserDefaults *defaults = [[NSUserDefaults alloc] initWithSuiteName:domainString];
-    [defaults setFloat:sender.value forKey:key];
-    [defaults synchronize];
-    valueLabel.text = [NSString stringWithFormat:@"%.2f", sender.value];
-    [self postNotification];
-}
-
-- (void)stepperChanged:(UIStepper *)sender {
-    NSString *key = sender.accessibilityLabel;
-    UILabel *valueLabel = objc_getAssociatedObject(sender, "valueLabel");
-    NSUserDefaults *defaults = [[NSUserDefaults alloc] initWithSuiteName:domainString];
-    [defaults setFloat:sender.value forKey:key];
-    [defaults synchronize];
-    valueLabel.text = [NSString stringWithFormat:@"%.2f", sender.value];
-    [self postNotification];
-}
-
-- (void)postNotification {
-    CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(),
-                                         CFSTR("com.schlub51.fipad.changed"),
-                                         NULL, NULL, YES);
-}
-
 -(void)sbreload {
     UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"fipad"
                             message:@"设置已保存\n现在 Respring 吗？"
@@ -164,9 +20,9 @@ NSString *const domainString = @"com.schlub51.fipad";
     UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"否" style:UIAlertActionStyleCancel handler:nil];
     UIAlertAction* yes = [UIAlertAction actionWithTitle:@"是" style:UIAlertActionStyleDestructive
         handler:^(UIAlertAction * action) {
-            NSTask *task = [[NSTask alloc] init];
-            [task setLaunchPath:ROOT_PATH_NS(@"/usr/bin/sbreload")];
-            [task launch];
+            NSTask *t = [[NSTask alloc] init];
+            [t setLaunchPath:ROOT_PATH_NS(@"/usr/bin/sbreload")];
+            [t launch];
         }];
     [alert addAction:defaultAction];
     [alert addAction:yes];
